77b6a6858b8065428aa9db34dd78464a9adedf6b
Redoblue <redoblue@hotmail.com>
2017-09-22 14:44:32 +0800

    basic success

--- ant.py
+++ ant.py
@@ -34,62 +34,23 @@ class Ant(pygame.sprite.Sprite):
 
         print(self.eta_left, self.arrived_pids)
         if self._arrived() and self.pid_next != self.arrived_pids[-1]:
+            self.arrived_pids.append(self.pid_next)
 
             if 'F' in g_graph.i2c[self.pid_next]:
+                g_graph.ants_in_F[self.pid_next] = True
                 self.wait = True
                 return
 
-            self.arrived_pids.append(self.pid_next)
             for p in np.random.permutation(g_graph.after[self.pid_next]):
-
-                if 'F' in g_graph.i2c[p] and not g_graph.ants_in_F[p] and \
-                                g_graph.ants_on_normal_road[self.pid_prev, p].qsize() < 1:
-                    self.pid_prev = self.pid_next
-                    self.pid_next = p
-                    self.pos = g_graph.nodes_position[self.pid_prev]
-                    self.eta = g_graph.nodes_distance[self.pid_prev, self.pid_next] / self._speed('normal')
-                    self.eta_left = self.eta
-                    self.direction = utils.get_direction(g_graph, self.pid_prev, self.pid_next)
-                    g_graph.ants_in_F[p] = True
-                    g_graph.ants_on_normal_road[self.pid_prev, self.pid_next].append(self)
-                    break
-
-                # print(globals.g_graph.i2c[self.pid_next], globals.g_graph.i2c[p])
-                if g_graph.flags_main_road[self.pid_prev, self.pid_next]:
-                    g_graph.ants_on_main_road[self.pid_prev, self.pid_next, self.previous_side].remove(self)
-                    anti_ants0 = g_graph.ants_on_main_road[self.pid_next, self.pid_prev, 0]
-                    anti_ants1 = g_graph.ants_on_main_road[self.pid_next, self.pid_prev, 1]
-                    # if both ways are blocked, wait
-                    if anti_ants0.qsize() > 0 and anti_ants1.qsize() > 0:
-                        continue
-                    # if either is clear
-                    self.pid_prev = self.pid_next
-                    self.pid_next = p
-                    self.pos = g_graph.nodes_position[self.pid_prev]
-                    self.eta = g_graph.nodes_distance[self.pid_prev, self.pid_next] / self._speed('main')
-                    self.eta_left = self.eta
-                    self.direction = utils.get_direction(g_graph, self.pid_prev, self.pid_next)
-                    if anti_ants0.qsize() > 0:
-                        self.previous_side = 0
-                        g_graph.ants_on_main_road[self.pid_prev, self.pid_next, 1].append(self)
-                    else:
-                        self.previous_side = 1
-                        g_graph.ants_on_main_road[self.pid_prev, self.pid_next, 0].append(self)
-                    break
-                else:
-                    g_graph.ants_on_normal_road[self.pid_prev, self.pid_next].remove(self)
-                    anti_ants = g_graph.ants_on_normal_road[self.pid_next, self.pid_prev]
-                    if anti_ants.qsize() > 0:
-                        continue
-                    else:
-                        self.pid_prev = self.pid_next
-                        self.pid_next = p
-                        self.pos = g_graph.nodes_position[self.pid_prev]
-                        self.eta = g_graph.nodes_distance[self.pid_prev, self.pid_next] / self._speed('normal')
-                        self.eta_left = self.eta
-                        self.direction = utils.get_direction(g_graph, self.pid_prev, self.pid_next)
-                        g_graph.ants_on_normal_road[self.pid_prev, self.pid_next].append(self)
-                        break
+                if g_graph.ants_in_F[p] == True:
+                    continue
+                self.pid_prev = self.pid_next
+                self.pid_next = p
+                self.pos = g_graph.nodes_position[self.pid_prev]
+                self.eta = g_graph.nodes_distance[self.pid_prev, self.pid_next] / self._speed()
+                self.eta_left = self.eta
+                self.direction = utils.get_direction(g_graph, self.pid_prev, self.pid_next)
+                break
 
         if self.direction is not None:
             interp = self.eta_left / self.eta
@@ -99,8 +60,8 @@ class Ant(pygame.sprite.Sprite):
             self.eta_left -= 0.01
             print(self.eta_left)
 
-    def _speed(self, rtype):
-        if rtype == 'main':
+    def _speed(self):
+        if g_graph.flags_main_road[self.pid_prev, self.pid_next]:
             return config.get_int('speed_{}_main'.format(self.atype))
         else:
             return config.get_int('speed_{}_normal'.format(self.atype))
